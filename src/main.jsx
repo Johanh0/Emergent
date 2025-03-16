c3304005a359e7f8104d9570bfb3c2ad6fb29388
Johan Herrera <69118220+Johanh0@users.noreply.github.com>
2025-03-16 13:20:32 -0400

    Revert "Fixing header and  footer  (#10)"
    This reverts commit b232b98928d6707266d5334f835b182feeef6511.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import UserProvider from "./context/UserProvider.jsx";
-import "./index.css";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
